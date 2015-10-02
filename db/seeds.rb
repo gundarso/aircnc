88f4855a9a20e6ce207077147ef05b93d370242d
Gundars Ostrovskis <gundarso@gmail.com>
2015-10-02 15:13:19 +0200

    seeding with cars

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,16 @@ Category.create(name: "Day to day car")
 Category.create(name: "Family car")
 Category.create(name: "Minivan")
 Category.create(name: "Supercar")
+
+20.times do
+  Car.create(
+    mileage: Faker::Number.number(6),
+    fuel: (['petrol', 'diesel', 'hybrid', 'electric']).max,
+    seats: rand(2..7),
+    pickup_address: Faker::Address.street_address + ', ' + Faker::Address.city,
+    category_id: rand(1..6),
+    rate: Faker::Number.number(3),
+    latitude: Faker::Number.decimal(7),
+    longitude: Faker::Number.decimal(7)
+    )
+end
